16ed503a780a3a3ae70c35e85734f78e6b69e026
Hernán R <hernan@null.net>
2026-05-01 21:45:30 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cd pseint
 # make clean ARCH=lnx
-make GPP=emcc   CXXFLAGS="-O3 -DNDEBUG -std=c++17 -DUSE_ZOCKETS"   LDFLAGS="-s EXPORT_ALL=1 -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS=['_main'] -s EXPORTED_RUNTIME_METHODS=['callMain','ccall','cwrap']"   ARCH=lnx
+make GPP=emcc   CXXFLAGS="-O3 -DNDEBUG -std=c++17 -DUSE_ZOCKETS"   LDFLAGS="-s EXPORT_ALL=1 -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 -s ASYNCIFY=1 -s ASYNCIFY_STACK_SIZE=1048576 -s ASYNCIFY_IMPORTS=['fd_read'] -s EXPORTED_FUNCTIONS=['_main','_pseint_run_editor'] -s EXPORTED_RUNTIME_METHODS=['callMain','ccall','cwrap','FS']"   ARCH=lnx
 cd ..
 cp bin/bin/pseint ../pseint-web/public/pseint.js
 cp bin/bin/pseint.wasm ../pseint-web/public/pseint.wasm
